d0c82ff32b44d56c03d0e92fa9f46990fc9e49d0
Phil <zgrubby@gmail.com>
2017-02-23 16:36:55 +0800

    add wrap handler

--- handler.go
+++ handler.go
@@ -1,6 +1,16 @@
 package zen
 
+import (
+	"net/http"
+)
+
 type (
 	// HandlerFunc is a type alias for handler
 	HandlerFunc func(*Context)
 )
+
+func wrapHandler(h http.HandlerFunc) HandlerFunc {
+	return func(c *Context) {
+		h(c.rw, c.req)
+	}
+}
