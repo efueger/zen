2c76e1e6e7ee02eef86df72d7f04634d4a48c734
Phil <zgrubby@gmail.com>
2017-02-26 16:06:49 +0800

    remove log

--- params.go
+++ params.go
@@ -1,16 +1,12 @@
 package zen
 
-import (
-	"log"
-	"strings"
-)
+import "strings"
 
 func (r *route) parseParams(c *Context) {
 	pattern := c.Req.URL.Path
 	pattern = strings.TrimSuffix(strings.TrimPrefix(pattern, "/"), "/")
 	parts := strings.Split(pattern, "/")
-	log.Println("parts", parts)
-	log.Println(r.params)
+
 	for i, k := range r.params {
 		c.params[k] = parts[i]
 	}
